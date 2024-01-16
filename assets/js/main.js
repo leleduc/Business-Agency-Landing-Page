6de00106d808a438f7d3b37bbb3e3ae085356ade
leleduc <leleduc@gmail.com>
2024-01-16 20:39:51 +0700

    ok

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+'use strict';
+
+jQuery(document).ready(function () {
+  /*
+   *    Navbar toggle for mobile
+   */
+  jQuery('[data-nav-toggler]').click(function () {
+    jQuery('[data-navbar]').toggleClass('active');
+    jQuery('[data-overlay]').toggleClass('active');
+    jQuery('body').toggleClass('nav-active');
+  });
+
+  /*
+   *    HEADER
+   *    Active Header when scroll down 100px
+   */
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 100) {
+      $('[data-header]').addClass('active');
+    } else {
+      $('[data-header]').removeClass('active');
+    }
+  });
+
+  /*
+   *    SLIDER
+   *
+   */
+  var slideCount = $('.slider-item').length;
+  // console.log(slideCount);
+  var slideWidth = $('.slider-item').outerWidth();
+  var gapValue = parseInt($('[data-slider-container]').css('gap'));
+  var currentSlide = 1;
+
+  jQuery('[data-slider-next]').click(function () {
+    if (currentSlide < slideCount) {
+      currentSlide++;
+    } else {
+      currentSlide = 1;
+    }
+    var slidePosition = -((currentSlide - 1) * (slideWidth + gapValue));
+    $('[data-slider-container]').css(
+      'transform',
+      'translateX(' + slidePosition + 'px)'
+    );
+  });
+
+  $('[data-slider-prev]').click(function () {
+    if (currentSlide > 1) {
+      currentSlide--;
+    } else {
+      currentSlide = slideCount;
+    }
+    var slidePosition = -((currentSlide - 1) * (slideWidth + gapValue));
+    $('[data-slider-container]').css(
+      'transform',
+      'translateX(' + slidePosition + 'px)'
+    );
+  });
+
+  jQuery('[data-slider-prev]').click(function () {});
+});
